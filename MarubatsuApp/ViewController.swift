15293583823bb28a951e42e9972fbbb78799666f
Akihito2523 <akihito0141@gmail.com>
2022-10-22 08:46:04 +0900

    Initial Commit

---
+++ MarubatsuApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarubatsuApp
+//
+//  Created by 鳥山彰仁 on 2022/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
